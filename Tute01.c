9e89dedc014b8f0e40e961a54eb4e2df146a5474
it21051920 <87510340+IT21051920@users.noreply.github.com>
2021-07-20 05:41:16 +0000

    Complited..!!!

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,20 @@
 #include <stdio.h>
 
 int main() {
+
+  int mark1,mark2;
+  float avg;
+
+  printf("Enter Mark 1 :");
+  scanf("%d",&mark1);
+
+  printf("Enter Mark 2 :");
+  scanf("%d",&mark2);
+
+  avg=(mark1+mark2)/2.0;
+
+  printf("Average =%.2f",avg);
+
   
   return 0;
 }
